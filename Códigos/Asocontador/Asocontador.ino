3002a25ddf37a891b1b8824c8ac03780fe56ccc1
Hector J Estrada Toledo <hjestrada@misena.edu.co>
2023-11-04 17:25:15 -0500

    Funcionando MQTT

--- Códigos/Asocontador/Asocontador.ino
+++ Códigos/Asocontador/Asocontador.ino
@@ -1,6 +1,7 @@
 #include <LiquidCrystal.h>
 #include <PubSubClient.h>
-#include <WiFi.h>
+#include <ArduinoJson.h>
+#include <WiFiManager.h>  // Incluye la biblioteca WiFiManager
 
 LiquidCrystal lcd(3, 2, 4, 5, 6, 7);
 
@@ -19,19 +20,21 @@ float calibracionTurbidezB = 0.0;
 float calibracionCloroK = 1.0;
 float calibracionCloroB = 0.0;
 
-const char* ssid = "Teranet Estrada";
-const char* password = "pitalito2023@";
 const char* mqttServer = "broker.hivemq.com";
 const int mqttPort = 1883;
 const char* mqttUser = "";
 const char* mqttPassword = "";
-const char* mqttTopic = "Asocontador/lectura";
+const char* mqttTopic = "Asocontador/MonitorPrincipal";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 void setup() {
-//set the resolution to 10 bits (0-1024)
+  // Inicia WiFiManager para configuración de WiFi
+  WiFiManager wifiManager;
+  wifiManager.autoConnect("ConfigurarWiFi");
+  
+  //set the resolution to 10 bits (0-1024)
   analogReadResolution(10);
   lcd.begin(16, 2);
   lcd.setCursor(2, 0);
@@ -48,7 +51,6 @@ void setup() {
   Serial.begin(9600);
   setup_wifi();
   client.setServer(mqttServer, mqttPort);
-  
 }
 
 void loop() {
@@ -62,33 +64,30 @@ void loop() {
   float chlorineValue = measureChlorine();
   displayValues(phValue, turbidityValue, chlorineValue);
 
-  char message[50];
-  snprintf(message, sizeof(message), "pH:%.2f, Turb:%.2f, Cl:%.2f", phValue, turbidityValue, chlorineValue);
+  DynamicJsonDocument doc(256);
+  doc["pH"] = phValue;
+  doc["Turbidity"] = turbidityValue;
+  doc["Chlorine"] = chlorineValue;
 
-  client.publish(mqttTopic, message);
-  // Impresión de los datos en el puerto serie
+  char jsonBuffer[256];
+  serializeJson(doc, jsonBuffer);
+
+  client.publish(mqttTopic, jsonBuffer);
   Serial.println("Datos enviados al broker MQTT:");
-  Serial.println(message);
+  Serial.println(jsonBuffer);
 
   delay(1000);  // Actualizar los valores cada segundo
 }
 
 void setup_wifi() {
-  delay(10);
-  // Conexión WiFi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Conexión WiFi...");
-  }
-  Serial.println("Conexión WiFi exitosa");
+  // La configuración de WiFi ahora se maneja a través de WiFiManager
+  // No es necesario repetirla aquí.
 }
 
 void reconnect() {
-  // Bucle hasta que estemos reconectados
   while (!client.connected()) {
     Serial.print("Conexión MQTT...");
-    if (client.connect("ESP32Client", mqttUser, mqttPassword)) {
+    if (client.connect("Monitor", mqttUser, mqttPassword)) {
       Serial.println("Conectado al broker MQTT");
     } else {
       Serial.print("Error de conexión, rc=");
@@ -120,7 +119,7 @@ float measurePH() {
     avgValue += buf[i];
   }
 
- float phValue = (float)avgValue * 3.3 / 1024 / 6;
+  float phValue = (float)avgValue * 3.3 / 1024 / 6;
   phValue = 3.5 * phValue;
 
   return phValue;
@@ -130,7 +129,6 @@ float measureTurbidity() {
   int sensorValue = analogRead(TurbidityPin);
   float voltage = sensorValue * (5.0 / 1024.0);
 
-  // Ajustar la calibración para convertir a UNT
   float turbidity = calibracionTurbidezK * voltage + calibracionTurbidezB;
 
   return turbidity;
@@ -140,26 +138,25 @@ float measureChlorine() {
   int sensorValue = analogRead(ChlorinePin);
   float voltage = sensorValue * (5.0 / 1024.0);
 
-  // Ajustar la calibración según tu sensor de cloro residual
   float chlorine = calibracionCloroK * voltage + calibracionCloroB;
 
   return chlorine;
 }
 
 void displayValues(float pH, float turbidity, float chlorine) {
-  lcd.setCursor(0, 0);  // Primera fila
+  lcd.setCursor(0, 0);
   lcd.print("pH: ");
   lcd.print(pH, 2);
   lcd.setCursor(11, 0);
   lcd.print("Unid");
-  lcd.setCursor(0, 1);  // Segunda fila
+  lcd.setCursor(0, 1);
   lcd.print("Turb: ");
   lcd.print(turbidity, 2);
   lcd.setCursor(12, 1);
   lcd.print("UNT");
   delay(5000);
   lcd.clear();
-  lcd.setCursor(0, 0);  // Mostrar el cloro residual
+  lcd.setCursor(0, 0);
   lcd.print("Cl: ");
   lcd.print(chlorine, 2);
   lcd.setCursor(10, 0);
