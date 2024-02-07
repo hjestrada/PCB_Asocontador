a4bcdc340e8db7465ce4774545bc05a527824f05
Hector J Estrada Toledo <hjestrada@misena.edu.co>
2024-02-07 12:55:27 -0500

    CAmibios

--- Códigos/Asocontador-7-2-2024/Asocontador-7-2-2024.ino
+++ Códigos/Asocontador-7-2-2024/Asocontador-7-2-2024.ino
@@ -1,8 +1,7 @@
 #include <LiquidCrystal.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
-#include <WiFiManager.h>  // Incluye la biblioteca WiFiManager
-#include <math.h>
+#include <WiFiManager.h>
 
 LiquidCrystal lcd(3, 2, 4, 5, 6, 7);
 
@@ -15,14 +14,13 @@ unsigned long int avgValue;
 float b;
 int buf[10], temp;
 
-// Calibración para la conversión de voltaje a UNT (turbidez)
 float calibracionTurbidezK = 1.0;
 float calibracionTurbidezB = 0.0;
 
-// Calibración para la conversión de voltaje a mg/L (cloro residual)
 float calibracionCloroK = 1.0;
 float calibracionCloroB = 0.0;
 
+float phValue, turbidityValue, chlorineValue;
 const char* mqttServer = "broker.hivemq.com";
 const int mqttPort = 1883;
 const char* mqttUser = "";
@@ -33,11 +31,24 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 
 void setup() {
+ 
+VerificacionInicial();
+  // Intenta conectar o crea un punto de acceso si no puede conectar
+  WiFiManager wifiManager;
+  if (!wifiManager.autoConnect("Asocontador_Config")) {
+    Serial.println("Fallo al conectar al WiFi. Reinicia el dispositivo y configura manualmente.");
+    delay(5000);
+    Sininternet();
+    ESP.restart();  // Cambio aquí: 'reset()' a 'restart()'
+    delay(1000);
+  }
 
-
-  //set the resolution to 10 bits (0-1024)
+  // Una vez conectado, muestra la dirección IP del punto de acceso (hotspot)
+  Serial.begin(9600);
+  Serial.println("Conectado al WiFi. IP del hotspot:");
+  Serial.println(WiFi.softAPIP());
   analogReadResolution(10);
-  lcd.begin(16, 2);
+  lcd.clear();
   lcd.setCursor(2, 0);
   lcd.print("Tecnoparque");
   lcd.setCursor(2, 1);
@@ -49,39 +60,58 @@ void setup() {
   lcd.setCursor(2, 1);
   lcd.print("ASOCONTADOR");
   lcd.clear();
-  Serial.begin(9600);
-    // Inicia WiFiManager para configuración de WiFi
-  WiFiManager wifiManager;
-  wifiManager.autoConnect("Asocontador_Config");
   client.setServer(mqttServer, mqttPort);
 }
-
+void VerificacionInicial(){
+ lcd.begin(16, 2);
+ lcd.setCursor(0, 0);
+  lcd.print("Sin Internet");
+  lcd.setCursor(0, 1);
+  lcd.print("Error al iniciar");
+  delay(2000);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Verificar Wifi");
+  lcd.setCursor(0, 1);
+  lcd.print("reinicie Tarjeta");
+  delay(2000);
+   }
 void loop() {
+
   if (!client.connected()) {
     reconnect();
+    displayValues(phValue, turbidityValue, chlorineValue);
+    Sininternet();
   }
 
   lcd.setCursor(0, 0);
-  float phValue = measurePH();
-  float turbidityValue = measureTurbidity();
-  float chlorineValue = measureChlorine();
+  phValue = measurePH();
+  turbidityValue = measureTurbidity();
+  chlorineValue = measureChlorine();
   displayValues(phValue, turbidityValue, chlorineValue);
 
-  DynamicJsonDocument doc(256);
-  doc["pH"] = phValue;
-  doc["Turbidity"] = turbidityValue;
-  doc["Chlorine"] = chlorineValue;
+  if (client.connected()) {
+    DynamicJsonDocument doc(256);
+    doc["pH"] = phValue;
+    doc["Turbidity"] = turbidityValue;
+    doc["Chlorine"] = chlorineValue;
 
-  char jsonBuffer[256];
-  serializeJson(doc, jsonBuffer);
+    char jsonBuffer[256];
+    serializeJson(doc, jsonBuffer);
 
-  client.publish(mqttTopic, jsonBuffer);
-  Serial.println("Datos enviados al broker MQTT:");
-  Serial.println(jsonBuffer);
+    client.publish(mqttTopic, jsonBuffer);
+    Serial.println("Datos enviados al broker MQTT:");
+    Serial.println(jsonBuffer);
+  }
 
-  delay(1000);  // Actualizar los valores cada segundo
+  delay(1000);
 }
-
+void Sininternet() {
+  lcd.setCursor(0, 0);
+  lcd.print("Sin Internet");
+  delay(2000);
+  lcd.clear();
+}
 void reconnect() {
   while (!client.connected()) {
     Serial.print("Conexión MQTT...");
@@ -91,58 +121,30 @@ void reconnect() {
       Serial.print("Error de conexión, rc=");
       Serial.print(client.state());
       Serial.println(" Intentando de nuevo en 5 segundos");
+      displayValues(phValue, turbidityValue, chlorineValue);
       delay(5000);
+      Sininternet();
     }
   }
 }
 
 float measurePH() {
-   float pH;
-  // Lee el valor de voltaje en el pin A0
+  float pH;
   float voltage = analogRead(SensorPin) * (5.0 / 1024.0);
   pH = 1.4151 * (voltage) + 2.9487;
-   return pH;
+  return pH;
 }
 
 float measureTurbidity() {
-    // float turbidez;
-   //    float voltage = analogRead(Turbidy_sensor) * (5.0 / 1024.0);
-
- // turbidez = -108.61 * (voltage) + 1148.1;
- // return turbidez;
-float turbidez;
- Tension = 0;  
-  Tension = analogRead(Turbidy_sensor)/1024*5; // Mapeo de la lectura analógica  
-  //Para compensar el ruido producido en el sensor tomamos 500 muestras y obtenemos la media  
-  for(int i=0; i<500; i++)  
-    {  
-      Tension += ((float)analogRead(Turbidy_sensor)/1024)*5;  
-    }  
-    Tension = Tension/500;  
-    Tension = redondeo(Tension,1);  
-    //Para ajustarnos a la gráfica de la derecha  
-    if(Tension < 2.5){  
-      turbidez = 3000;  
-    }else{  
-             turbidez = -1120.4 * (Tension * Tension) + 5742.3 * Tension - 4352.9;   
-  
-    }  
-return turbidez;
+  float turbidez;
+  float voltage = analogRead(Turbidy_sensor) * (5.0 / 1024.0);
+  turbidez = (-108.61 * (voltage) + 1148.1)/1000;
+  return turbidez;
 }
 
 float measureChlorine() {
- float chlorine=0; //Cambiar Al momento de conectar el sensor.
- // int sensorValue = analogRead(ChlorinePin);
-//  float voltage = sensorValue * (5.0 / 1024.0);
- // float chlorine = calibracionCloroK * voltage + calibracionCloroB;
+  float chlorine = 0;
   return chlorine;
-}
-
-float redondeo(float p_entera, int p_decimal)
-{
-  float multiplicador = powf( 10.0f, p_decimal);  //redondeo a 2 decimales
-  p_entera = roundf(p_entera * multiplicador) / multiplicador;
-  return p_entera;
 }
 
 void displayValues(float pH, float turbidity, float chlorine) {
